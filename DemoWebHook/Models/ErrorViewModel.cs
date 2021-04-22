b3e4945fc37e1e67f88275280f8206dfc516d006
Aniel Cruz <aniel.bispo@gmail.com>
2021-04-22 16:52:38 -0300

    Add project files.

---
+++ DemoWebHook/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoWebHook.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ webhookTest/Controllers/MessagingController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Twilio.AspNet.Common;
+using Twilio.AspNet.Core;
+using Twilio.TwiML;
+
+namespace webhookTest.Controllers
+{
+    public class MessagingController : TwilioController
+    {
+        [HttpPost]
+        public TwiMLResult Index(SmsRequest request)
+        {
+            var response = new MessagingResponse();
+            response.Message("Hello world!");
+            return TwiML(response);
+        }
+    }
+}
