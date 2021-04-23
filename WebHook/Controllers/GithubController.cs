7088743f8be45713eb36e382880169d7e3058090
Aniel Cruz <aniel.bispo@gmail.com>
2021-04-22 21:15:41 -0300

    Webhook com o 4.8

---
+++ WebHook/Controllers/GithubController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebHooks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebHook.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GithubController : ControllerBase
+    {   
+        [HttpPost]
+        public IActionResult GitHubHandler(JObject data)
+        {
+            var result = JsonConvert.SerializeObject(data);
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Tudo Certo!");
+        }
+    }
+}
+
+
---
+++ webhookTest/Controllers/GithubController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebHooks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webhookTest.Controllers
+{
+    [Route("wb")]
+    [ApiController]
+    public class GithubController : ControllerBase
+    {
+        [GitHubWebHook]
+        [HttpPost]
+        public IActionResult GitHubHandler(JObject data)
+        {
+            var result = JsonConvert.SerializeObject(data);
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Tudo Certo!");
+        }
+    }
+}
